626a418d7174b71913435e303076080b4cdc0a25
osama <sosamaalirizvi@gmail.com>
2020-08-03 02:31:33 +0500

    minor changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "8927e54e2830a96d5da1705eee366018",
+  "main.dart.js": "fbd3e45f8e0a60772595765a80ecdb8c",
 "manifest.json": "615cf78c2e91ec0b1792488fd59ef71d",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
